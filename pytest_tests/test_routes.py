69439eb560c07e95c7b6bb30967e74aea2966411
Karestwww <Karestwww@yandex.ru>
2024-05-31 09:27:14 +0300

    Тестирование

---
+++ pytest_tests/test_routes.py
@@ -0,0 +1,38 @@
+# test_routes.py
+from http import HTTPStatus
+
+from django.urls import reverse
+import pytest
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+# test_routes.py
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(admin_client, name):
+    url = reverse(name)
+    response = admin_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+
