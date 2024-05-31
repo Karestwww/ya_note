c999d6afe0e01da070da971dbad46e7ba7137a27
Karestwww <Karestwww@yandex.ru>
2024-05-31 11:05:00 +0300

    Перед завершением теории, последний рывок

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,135 @@
+# test_routes.py
+import pytest
+
+from django.urls import reverse
+from http import HTTPStatus
+from pytest_django.asserts import assertRedirects
+
+from notes.forms import NoteForm
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+# test_routes.py
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+# Параметризуем тестирующую функцию:
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    # Предварительно оборачиваем имена фикстур 
+    # в вызов функции pytest.lazy_fixture().
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status 
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:detail', pytest.lazy_fixture('slug_for_args')),
+        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
+        ('notes:delete', pytest.lazy_fixture('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+# Передаём в тест анонимный клиент, name проверяемых страниц и args:
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
+
+
+@pytest.mark.parametrize(
+    # Задаём названия для параметров:
+    'parametrized_client, note_in_list',
+    (
+        # Передаём фикстуры в параметры при помощи "ленивых фикстур":
+        (pytest.lazy_fixture('author_client'), True),
+        (pytest.lazy_fixture('not_author_client'), False),
+    )
+)
+def test_notes_list_for_different_users(
+        # Используем фикстуру заметки и параметры из декоратора:
+        note, parametrized_client, note_in_list
+):
+    url = reverse('notes:list')
+    # Выполняем запрос от имени параметризованного клиента:
+    response = parametrized_client.get(url)
+    object_list = response.context['object_list']
+    # Проверяем истинность утверждения "заметка есть в спис
+
+
+@pytest.mark.parametrize(
+    # В качестве параметров передаём name и args для reverse.
+    'name, args',
+    (
+        # Для тестирования страницы создания заметки 
+        # никакие дополнительные аргументы для reverse() не нужны.
+        ('notes:add', None),
+        # Для тестирования страницы редактирования заметки нужен slug заметки.
+        ('notes:edit', pytest.lazy_fixture('slug_for_args'))
+    )
+)
+def test_pages_contains_form(author_client, name, args):
+    # Формируем URL.
+    url = reverse(name, args=args)
+    # Запрашиваем нужную страницу:
+    response = author_client.get(url)
+    # Проверяем, есть ли объект формы в словаре контекста:
+    assert 'form' in response.context
+    # Проверяем, что объект формы относится к нужному классу.
+    assert isinstance(response.context['form'], NoteForm)
+
+
